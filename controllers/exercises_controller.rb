1fa7edd66ba626393505b9aedabd27bc3f531812
mikethorpe <michael.a.thorpe@gmail.com>
2018-07-23 16:43:10 +0100

    Adding functionality to create exercises

--- controllers/exercises_controller.rb
+++ controllers/exercises_controller.rb
@@ -5,3 +5,14 @@ get '/exercises' do
     @all_exercises = Exercise.find_all()
     erb(:"exercises/index")
 end
+
+# new
+get '/exercises/new' do
+    erb(:"exercises/new")
+end
+
+post '/exercises' do
+    exercise = Exercise.new(params)
+    exercise.save()
+    redirect to ("/exercises")
+end
