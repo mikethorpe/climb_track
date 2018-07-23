7429638d3f954487c925cd33db69f367db316504
mikethorpe <michael.a.thorpe@gmail.com>
2018-07-23 20:14:48 +0100

    Added views and controller functions to create, delete and edit exercises

--- controllers/exercises_controller.rb
+++ controllers/exercises_controller.rb
@@ -1,12 +1,12 @@
 require_relative('../models/exercise')
 
-# exercises
+# index
 get '/exercises' do
     @all_exercises = Exercise.find_all()
     erb(:"exercises/index")
 end
 
-# new
+# create
 get '/exercises/new' do
     erb(:"exercises/new")
 end
@@ -14,5 +14,30 @@ end
 post '/exercises' do
     exercise = Exercise.new(params)
     exercise.save()
+    redirect to ("/exercises")
+end
+
+#  delete
+get '/exercises/:id/delete' do
+    @exercise = Exercise.find(params['id'])
+    erb(:"exercises/delete")
+end
+
+post '/exercises/:id/delete' do
+    exercise = Exercise.find(params['id'])
+    exercise.delete()
+    redirect to ("/exercises")
+end
+
+# edit
+get '/exercises/:id/edit' do
+    @exercise = Exercise.find(params['id'])
+    erb(:"exercises/edit")
+end
+
+# update
+post '/exercises/:id/edit' do
+    exercise = Exercise.new(params)
+    exercise.update()
     redirect to ("/exercises")
 end
