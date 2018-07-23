d7de69d2c346b314bf9f3b19b18cdad7486ef874
mikethorpe <michael.a.thorpe@gmail.com>
2018-07-23 11:07:17 +0100

    Updating columns in climb_track.sql

--- db/climb_track.sql
+++ db/climb_track.sql
@@ -3,7 +3,7 @@ DROP TABLE exercises;
 DROP TABLE workouts;
 
 CREATE TABLE exercises (
-    id INT8 PRIMARY KEY,
+    id SERIAL8 PRIMARY KEY,
     name VARCHAR(255),
     reps INT8,
     sets INT8,
@@ -11,13 +11,13 @@ CREATE TABLE exercises (
 );
 
 CREATE TABLE workouts (
-    id INT8 PRIMARY KEY,
+    id SERIAL8 PRIMARY KEY,
     name VARCHAR(255),
-    complete BOOLEAN NOT NULL
+    complete BOOLEAN
 );
 
 CREATE TABLE activities (
-    id INT8 PRIMARY KEY,
+    id SERIAL8 PRIMARY KEY,
     workout_id INT8 REFERENCES workouts(id),
     exercise_id INT8 REFERENCES exercises(id),
     result VARCHAR(255)
