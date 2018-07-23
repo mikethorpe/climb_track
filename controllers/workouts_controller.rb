2e19041065bf3edf4fe6a76f3902f88bf4f1234e
mikethorpe <michael.a.thorpe@gmail.com>
2018-07-23 21:17:36 +0100

    Added functionality to show future workout session details

--- controllers/workouts_controller.rb
+++ controllers/workouts_controller.rb
@@ -1,1 +1,8 @@
 require_relative('../models/workout')
+
+# show
+get '/workouts/:id' do
+    @workout = Workout.find(params['id'])
+    @exercises = @workout.exercises()
+    erb(:"workouts/show")
+end
--- models/workout.rb
+++ models/workout.rb
@@ -23,6 +23,16 @@ class Workout
         @id = result.first['id'].to_i
     end
 
+    def self.find(id)
+        sql = "SELECT * FROM workouts
+        WHERE workouts.id = $1"
+        values = [id]
+        result = SqlRunner.run(sql, values)
+        workout_hash = result.first()
+        return Workout.new(workout_hash)
+
+    end
+
     def self.find_all()
         sql = "SELECT * FROM workouts;"
         result = SqlRunner.run(sql)
