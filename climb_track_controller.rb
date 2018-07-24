284e97b53fcb0f050de04bb6285d8d737678b4a3
mikethorpe <michael.a.thorpe@gmail.com>
2018-07-24 23:14:52 +0100

    Added functionality to display a result icon next to workout overall result on past sessions view

--- climb_track_controller.rb
+++ climb_track_controller.rb
@@ -2,20 +2,20 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('controllers/exercises_controller.rb')
 require_relative('controllers/workouts_controller.rb')
+require_relative('models/image_map')
 
 also_reload('./models/*')
 
 #index
 get '/' do
     @all_workouts = Workout.find_all()
+    @result_icons = ImageMap.icons()
     erb(:index)
 end
 
 # future
 get '/future' do
     @all_workouts = Workout.find_all()
-    p "These are workouts"
-    @all_workouts.each{|workout| p workout}
     erb(:future)
 end
 
