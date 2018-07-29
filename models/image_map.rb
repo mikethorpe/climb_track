8a5be5d2252a4e35d0e1efff4f0814ea30a03271
mikethorpe <michael.a.thorpe@gmail.com>
2018-07-29 13:34:15 +0100

    Committing fixed link in image map

--- models/image_map.rb
+++ models/image_map.rb
@@ -1,11 +1,11 @@
 class ImageMap
     def self.icons
         return {
-            -1 => "/images/white_-1.jpg", 
-            0 => "/images/grey_0.jpg",
-            1 => "/images/red_1.jpg",
-            2 => "/images/amber_2.jpg",
-            3 => "/images/green_3.jpg"
+            -1 => "/images/clock.png", 
+            0 => "/images/cross.png",
+            1 => "/images/minus.png",
+            2 => "/images/muscle.png",
+            3 => "/images/plus.png"
         }
     end
 end
