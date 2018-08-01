8ea99b5602c9288c9d8c4e1d07e0d51fdef37ef7
mikethorpe <michael.a.thorpe@gmail.com>
2018-08-01 23:50:24 +0100

    Final staging for Heroku

---
+++ config.ru
@@ -0,0 +1,4 @@
+# config.ru
+
+require './climb_track_controller.rb
+run Sinatra::Application
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -7,14 +7,15 @@ class SqlRunner
     def self.run(sql, values = [])
         
         begin
-            db = PG.connect({dbname: 'climb_track', host: 'localhost'})
+            db = PG.connect({dbname: 'd3j9p3drbina54', host: 'ec2-54-235-177-183.compute-1.amazonaws.com',
+            port: 5432, user: 'wynmmomzjvpexn', password: '44f1e6d68c7b1c22fc9c4d7216cff72f9ea8bd113646c22bb4fb0af4882ac382'})
             db.prepare("query", sql)
             result = db.exec_prepared("query", values)
         ensure 
             db.close() if db !=nil
         end
 
-        return result
+        return result   
         
     end
 end
