7a3ebe934303260a642918207b1d9ebadd7e29d6
mikethorpe <michael.a.thorpe@gmail.com>
2018-08-03 14:53:42 +0100

    Deleting gemfiles, procfile and config.ru. Updating sql runner with heroku details

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,9 +5,9 @@ class SqlRunner
     def self.run(sql, values = [])
         
         begin
-            db = PG.connect( {dbname: 'ddv9dqgfhacosg',
-                host: 'ec2-50-16-195-131.compute-1.amazonaws.com',
-                port: 5432, user: 'zchicsmqeyppsm', password: 'b607972720b4dedf55dd8b4babf01a4455e192eea109b556df2846f46257c87a'})
+            db = PG.connect( {dbname: 'd76443mlq3h3sg',
+                host: 'ec2-54-217-235-16.eu-west-1.compute.amazonaws.com',
+                port: 5432, user: 'ymqsmjgyjxilqz', password: 'e2d633afdac8663995b75ca396230a21a3105456425e6fc40f28705f9be3a775'})
             db.prepare("query", sql)
             result = db.exec_prepared("query", values)
         ensure 
