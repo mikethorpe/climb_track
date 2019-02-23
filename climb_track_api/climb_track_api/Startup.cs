132e5a77bfda84aeca3fc9ffeac4b89e052e6231
Mike Thorpe <mike@coach-logic.com>
2019-02-23 22:49:02 +0000

    Updating server to use CORS header for development server

--- climb_track_api/climb_track_api/Startup.cs
+++ climb_track_api/climb_track_api/Startup.cs
@@ -27,6 +27,7 @@ namespace climb_track_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<ClimbTrackContext>(opt =>
                opt.UseInMemoryDatabase("ClimbTrack"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -35,6 +36,9 @@ namespace climb_track_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:3000"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
