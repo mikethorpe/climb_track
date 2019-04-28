4b0145668633ebf59ca104ce3f51d71020dc532b
Mike Thorpe <mike@coach-logic.com>
2019-04-28 09:09:00 +0100

    Removing storybook default stories

--- climb_track_client/src/stories/ExerciseForm.story.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import ExerciseForm from '../components/ExerciseForm';
-
-storiesOf('ExerciseForm', module)
-  .add('default', () => (
-    <ExerciseForm/>
-  ))
