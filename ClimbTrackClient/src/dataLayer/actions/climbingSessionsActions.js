fe11b890d0a4bf52a0db4003ab53fca4fd7bb7cc
Mike Thorpe <michael.a.thorpe@gmail.com>
2020-04-25 12:45:40 +0100

    Removing client

--- ClimbTrackClient/src/dataLayer/actions/climbingSessionsActions.js
+++
@@ -1,8 +0,0 @@
-import { CREATE_CLIMBING_SESSION } from './types';
-
-export const createClimbingSession = (climbingSession) => {
-    return {
-        type: CREATE_CLIMBING_SESSION,
-        payload: climbingSession
-    };
-};
--- ClimbTrackClient/src/dataLayer/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import climbingSessionReducer from './climbingSessionReducer';
-
-export default combineReducers({
-    climbingSessions: climbingSessionReducer
-});
