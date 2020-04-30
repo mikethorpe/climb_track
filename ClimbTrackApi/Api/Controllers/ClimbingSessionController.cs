27d6b79a6cd7d60f28208fbad67f1e2a9069c119
Mike Thorpe <michael.a.thorpe@gmail.com>
2020-04-30 19:30:08 +0100

    Fetching climbing sessions from back end

--- ClimbTrackApi/Api/Controllers/ClimbingSessionController.cs
+++ ClimbTrackApi/Api/Controllers/ClimbingSessionController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ClimbTrackApi.Domain.Models;
 using Domain.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClimbTrackApi.Api.Controllers
--- climbtrackclient/src/dataLayer/actions/climbingSessionsActions.js
+++ climbtrackclient/src/dataLayer/actions/climbingSessionsActions.js
@@ -1,22 +1,24 @@
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
-import { CREATE_CLIMBING_SESSION, SET_CLIMBING_SESSIONS } from './types';
+import { SET_CLIMBING_SESSIONS } from './types';
 import mockApi from '../mockApi/mockApi';
 
 export const useCreateClimbingSession = () => {
     const dispatch = useDispatch();
     return async (climbingSession) => {
         await axios.post('/api/climbingsession', climbingSession);
-        // dispatch({ type: CREATE_CLIMBING_SESSION, payload: climbingSession });
+        const response = await axios.get('/api/climbingsession');
+        const fetchedClimbingSessions = response.data;
+        dispatch({ type: SET_CLIMBING_SESSIONS, payload: fetchedClimbingSessions });
     };
 };
 
 export const useFetchClimbingSessions = () => {
     const dispatch = useDispatch();
     return async () => {
-        const fetchedClimbingSessions = mockApi.climbingSessions;
-        // const response = await axios.get('/api/climbingsession');
-        // const fetchedClimbingSessions = response.data;
+        // const fetchedClimbingSessions = mockApi.climbingSessions;
+        const response = await axios.get('/api/climbingsession');
+        const fetchedClimbingSessions = response.data;
         dispatch({ type: SET_CLIMBING_SESSIONS, payload: fetchedClimbingSessions });
     }
 };
