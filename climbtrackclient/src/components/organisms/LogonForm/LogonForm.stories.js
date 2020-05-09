ff2fdf3b2eb7358b2eb7386fa75068fdf4281704
Mike Thorpe <michael.a.thorpe@gmail.com>
2020-05-09 21:05:51 +0100

    Fixing stories. Adding styled components. Adding storybook router.

--- climbtrackclient/src/components/organisms/LogonForm/LogonForm.stories.js
+++ climbtrackclient/src/components/organisms/LogonForm/LogonForm.stories.js
@@ -4,10 +4,12 @@ import { LogonForm } from './LogonForm';
 import { storiesOf } from '@storybook/react';
 import Provider from '../../../dataLayer/store/providerWrapper';
 import createStore from '../../../dataLayer/store/store';
+import StoryRouter from 'storybook-react-router';
 
 const store = createStore({});
 
 storiesOf('LogonForm', module)
+    .addDecorator(StoryRouter())
     .add('Default', () => (
         <Provider store={store}>
             <LogonForm />
