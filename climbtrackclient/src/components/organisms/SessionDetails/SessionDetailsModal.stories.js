3bab9616927bd1effb0e1a55d0d4973541833fbc
Mike Thorpe <michael.a.thorpe@gmail.com>
2020-06-06 11:16:54 +0100

    Updating session details story with mock store

--- climbtrackclient/src/components/organisms/SessionDetails/SessionDetailsModal.stories.js
+++ climbtrackclient/src/components/organisms/SessionDetails/SessionDetailsModal.stories.js
@@ -1,10 +1,36 @@
+import React from "react";
+import { SessionDetailsModal } from "./SessionDetailsModal";
+import { storiesOf } from "@storybook/react";
+import createStore from "../../../dataLayer/store/store";
+import Provider from "../../../dataLayer/store/providerWrapper";
 
-import React from 'react';
-import { SessionDetailsModal } from './SessionDetailsModal';
-import { storiesOf } from '@storybook/react';
-import createStore from '../../../dataLayer/store/store';
+const storeWithSelectedSessionAndStyles = {
+  userInterface: {
+    sessionDetailsModalDisplayed: true,
+  },
+  climbingSessions: {
+    selectedSession: {
+      id: 1,
+      dateTime: "22th April 2019",
+      maxGrade: "7b",
+      climbs: [
+        { id: 1234, grade: "7a", style: { id: 1, description: "Overhang" } },
+        { id: 345215, grade: "7b", style: { id: 2, description: "Slab" } },
+      ],
+    },
+  },
+  styles: [
+    { id: 1, description: "Overhang" },
+    { id: 2, description: "Slab" },
+    { id: 3, description: "Arete" },
+    { id: 4, description: "Crimpy" },
+  ],
+};
 
-storiesOf('SessionDetails', module)
-    .add('Default', () => (
-        <SessionDetailsModal />
-    ));
+const store = createStore(storeWithSelectedSessionAndStyles);
+
+storiesOf("SessionDetails", module).add("Default", () => (
+  <Provider store={store}>
+    <SessionDetailsModal />
+  </Provider>
+));
